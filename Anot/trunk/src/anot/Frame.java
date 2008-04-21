6adf96c6510954936ed7c8915b810912d0ee4040
tgwizard <tgwizard@e3dc7b9c-8c4a-0410-9f97-d3ef1dda7e21>
2008-04-21 17:14:41 +0000

    change of path of activities.xml

--- Anot/trunk/src/anot/Frame.java
+++ Anot/trunk/src/anot/Frame.java
@@ -68,7 +68,7 @@ public class Frame extends javax.swing.JFrame {
         
         
         //TODO: error handling, popup-window etc.
-        activityStore = ActivityStoreBuilder.loadActivityStoreFromFile("data/activities.xml");
+        activityStore = ActivityStoreBuilder.loadActivityStoreFromFile("activities.xml");
         
         
         DiagramPanel diagramPanel = new DiagramPanel();
